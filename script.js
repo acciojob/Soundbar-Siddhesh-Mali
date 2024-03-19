455cf1a9c8b1e5b347b424b9f7b63ee3ceed08c2
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-19 11:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let applause = () => new Audio("./sounds/applause.mp3").play()
+let boo = () => new Audio("./sounds/boo.mp3").play()
+let gasp = () => new Audio("./sounds/gasp.mp3").play()
+let tada = () => new Audio("./sounds/tada.mp3").play()
+let victorywrong = () => new Audio("./sounds/victory.mp3").play()
+let wrong = () => new Audio("./sounds/wrong.mp3").play()
+let stop = () => location.reload();
